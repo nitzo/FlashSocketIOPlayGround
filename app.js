f1e23a54f6f9b0f9409d786ba2b853bdd10349c9
Nitzan Bar <nitzanbar@gmail.com>
2014-05-10 14:20:01 +0300

    Added broadcast support and send to a specific socketid

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-
-
 var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server)
     , redis = require('redis')
     , RedisStore = require('socket.io/lib/stores/redis')
-    ,argv = require('optimist').argv;
+    , argv = require('optimist').argv;
 
 var port = argv.port;
 port = port || 3005;
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index.ejs', { port : port});
-  //res.sendfile(__dirname + '/index.html');
+    res.render('index.ejs', { port: port});
+    //res.sendfile(__dirname + '/index.html');
 });
 
 
-app.get('/sockets', function(req, res){
+app.get('/sockets', function (req, res) {
     var data = {};
     data.connectedClients = io.sockets.clients();
 
@@ -27,29 +25,48 @@ app.get('/sockets', function(req, res){
 
 var pub = redis.createClient(), sub = redis.createClient(), client = redis.createClient();
 
-pub.auth('screemo', function (err) { if (err) throw err; });
-sub.auth('screemo', function (err) { if (err) throw err; });
-client.auth('screemo', function (err) { if (err) throw err; });
-
+pub.auth('screemo', function (err) {
+    if (err) throw err;
+});
+sub.auth('screemo', function (err) {
+    if (err) throw err;
+});
+client.auth('screemo', function (err) {
+    if (err) throw err;
+});
 
 
 io.set('store', new RedisStore({
-    redis : redis
-   ,redisPub : pub
-   ,redisSub : sub
-   ,redisClient : client
+    redis: redis, redisPub: pub, redisSub: sub, redisClient: client
 }));
 
 io.sockets.on('connection', function (socket) {
-  socket.set('port', port);
+    socket.set('port', port);
+    socket.join(socket.id);
 
-  socket.join('room');
+    socket.on('broadcast', function (data) {
+        console.log('BROADCAST Recieved! ' + JSON.stringify(data));
+        socket.broadcast.emit('message', data.message);
+        socket.emit('message', 'Message ' + data.message + ' was broadcasted!');
+    });
 
-  socket.on('data', function (data) {
-    console.log('DATA Recieved! '  + data);
-      socket.broadcast.to('room').emit('data', data);
-      socket.emit('data', 'OK!');
-  });
+
+    socket.on('message', function (data) {
+        console.log('Message Recieved! ' + JSON.stringify(data));
+
+        if (data.socketid){
+            socket.broadcast.to(data.socketid).emit('message', data.message + ' from ' + socket.id);
+            socket.emit('message', 'Message ' + data.message + ' was sent to ' + data.socketid);
+        }
+        else {
+            socket.emit('message', 'No socket specified!! ');
+        }
+
+
+
+    });
+
+
 });
 
 server.listen(port);
